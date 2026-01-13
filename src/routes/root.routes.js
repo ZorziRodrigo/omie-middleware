cc1ae22dcc4d1fa7e58c24fbf6ae5158da4a0ef6
ZorziRodrigo <rrrrzorzi@gmail.com>
2026-01-13 15:52:30 -0300

    Add root route for service status and available routes
    add root.routes.js

---
+++ src/routes/root.routes.js
@@ -0,0 +1,21 @@
+const { sendJson } = require('../utils/http');
+
+module.exports = function rootRoutes() {
+  return [
+    {
+      method: 'GET',
+      path: '/',
+      handler: async (_req, res) => {
+        return sendJson(res, 200, {
+          status: 'ok',
+          service: 'omie-middleware',
+          message: 'Middleware no ar',
+          routes: [
+            'GET /health',
+            'POST /audit/clientes/buscar'
+          ]
+        });
+      }
+    }
+  ];
+};
